1cfc5d6f9f9315ba0f83abd23e7ea0dc09d3aaa8
huijing-moon <kyong26264@gmail.com>
2022-05-28 23:52:30 +0900

    database 연동완료

---
+++ src/main/java/basic/Dao/BasicDao.java
@@ -0,0 +1,4 @@
+package basic.Dao;
+
+public class BasicDao {
+}
---
+++ src/main/java/basic/Dto/BasicDto.java
@@ -0,0 +1,13 @@
+package basic.Dto;
+
+import lombok.Data;
+
+@Data
+public class BasicDto {
+    private Integer id;
+    private String title;
+    private String name;
+    private Integer sequence;
+    private String type;
+    private String datetime;
+}
